e047d7c6358681fa25d26419bebe094e6a4cbfb3
Dominic Pfister <dominic.pfister@swissonline.ch>
2015-09-16 15:52:20 +0200

    logout :cancer: :door:

---
+++ Cashur-web/src/ch/cashur/web/controllers/LogoutController.java
@@ -0,0 +1,21 @@
+package ch.cashur.web.controllers;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+@ManagedBean
+@RequestScoped
+public class LogoutController implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private FacesContext facesContext = FacesContext.getCurrentInstance();
+    private HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
+	
+	public void logoutCustomer() {
+		session.setAttribute("isLoggedIn", false);
+		session.setAttribute("User", null);
+	}
+}
